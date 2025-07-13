14cbc556f01b83bf84f4fa6671dee434032fa3b2
Suwath mihindukulasooriya <suwathdaminda@yahoo.com>
2025-07-13 11:25:46 +1000

    #0002: adding typescript excersise - chapter2

---
+++ chapter-2/src/test2.ts
@@ -0,0 +1,31 @@
+// let a = Symbol('a')
+// console.log(a)
+// let b= symbol = Symbol('b')
+// let c = a === b
+// console.log(c)
+// const d: {b:number} = {
+//     b:10,
+//     // c : {
+//     //     f:'k'
+//     // }
+// }
+// console.log(d.b)
+// console.log(d.c.f)
+// let e = object = {
+//     x:'12'
+// }
+// console.log(e)
+let a: {b:number}
+a = {b:2}
+a = {
+    b:1
+    // c:2
+}
+// let i:number
+// i = 1
+// let j = i *3
+// console.log(j)
+
+let i = 3
+let j = i *  2   
+    
